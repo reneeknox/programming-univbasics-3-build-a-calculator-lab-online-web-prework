f709490c5fc1cd8336bffe4e98c1a3137d0aab50
Renee France <renee5.0@outlook.com>
2019-07-12 23:30:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,16 @@
-def addition(num1, num2)
-
+def=(addition)=(num1)+(num2)=(sum of(num1),(num2))do
+do 5+4=
 end
 
-def subtraction(num1, num2)
-
+def (subtraction)=(num1)-(num2)= (difference of (num1),num2))do
+ do 12-8=
 end
 
-def division(num1, num2)
-
+def (division)=(num1)/(num2)=(quotient of (num1),(num2)
+do 15/5=
 end
 
-def multiplication(num1, num2)
+def (multiplication)(num1)*(num2)
 
 end
 
