28293837d3bc99693c27ee27a4c4e37c88b9e141
Renee France <renee5.0@outlook.com>
2019-07-12 23:51:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
-def addition(num1, num2)
+def= math.(addition)=(num1)+(num2)= num3
+do 5+4=
+end
+
+def (subtraction)=(num1)-(num2)= (difference(num1),num2))
+ do 12-8=
+end
+
+def (division)=(num1)/(num2)=(quotient(num1),(num2)
+do 15/5=
+end
+
+def (multiplication)(num1)*(num2))=(sum(num1),(num2))
+  do 12*3=
 
 end
 
-def subtraction(num1, num2)
+def (modulo(num1)mod(num2))=(num3)
 
 end
 
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
+def (square_root(num)^
 
 end
